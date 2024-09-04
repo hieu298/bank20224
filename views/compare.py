f8fe82090456042848cf9597b1289a8cf2a08a2f
hieu298 <85894528+hieu298@users.noreply.github.com>
2024-09-04 13:08:55 +0700

    Update compare.py

--- views/compare.py
+++ views/compare.py
@@ -245,3 +245,8 @@ if a is not None and b is not None and c is not None and d is not None:
     st.plotly_chart(fig)
 else:
     st.write("Không có dữ liệu để hiển thị1.")
+
+e = taodf(all_banks,file_path, pick1)
+st.write(e)
+
+
